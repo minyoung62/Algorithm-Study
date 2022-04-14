ef6379e79629e2ca20d6043e482a634ae0a0b2ee
minyoung62 <tnd05106@naver.com>
2022-04-14 21:16:10 +0900

    몫 문제 한번 더 보기 !!!

---
+++ programmers/SummerWinterCoding/점프와순간이동.py
@@ -0,0 +1,17 @@
+def solution(n):
+    ans = 0
+    
+    while n > 0:
+        if n % 2 != 0:
+            n = (n - 1) / 2
+            ans += 1
+        else:
+            n /= 2
+
+    return ans
+
+print(solution(6))
+
+print(solution(5))
+
+print(solution(5000))
