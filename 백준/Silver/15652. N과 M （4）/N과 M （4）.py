24d142027e68fef39cc9fc171f96599050095861
minyoung <61530368+minyoung62@users.noreply.github.com>
2022-04-25 18:15:12 +0900

    [Silver III] Title: N과 M (4), Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,10 @@
+from itertools import *
+
+n, m = map(int, input().split())
+
+l = [i for i in range(1, n + 1)]
+
+al = combinations_with_replacement(l, m)
+
+for i in al:
+    print(" ".join(map(str, i)))
