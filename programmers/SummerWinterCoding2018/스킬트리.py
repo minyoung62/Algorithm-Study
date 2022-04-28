4d5457f5b62cef0dfa14a541f34edbcb5945696d
minyoung62 <tnd05106@naver.com>
2022-04-28 23:37:35 +0900

    no message

--- programmers/SummerWinterCoding2018/스킬트리.py
+++ programmers/SummerWinterCoding2018/스킬트리.py
@@ -1,27 +1,48 @@
 
 
+# def solution(skill, skill_trees):
+#     answer = 0
+#     print()
+#     for i in range(len(skill_trees)):
+#         tmp = "" 
+#         for k in skill_trees[i]:
+
+#             if k in skill :
+#                 tmp+=k
+
+#         if len(tmp) == 0:
+#             answer += 1
+#             continue
+
+#         p = skill.index(tmp[0])
+ 
+#         if skill[p:len(tmp)] == tmp:
+#             answer += 1
+
+#     return answer
+
 def solution(skill, skill_trees):
     answer = 0
-    print()
-    for i in range(len(skill_trees)):
-        tmp = "" 
-        for k in skill_trees[i]:
-
-            if k in skill :
-                tmp+=k
-
-        if len(tmp) == 0:
+    
+    for skill_tree in skill_trees:
+        
+        pre_skill = ""
+        flag = 1
+        for i in range(len(skill_tree)):
+            
+            if skill_tree[i] in skill:
+                pre_skill += skill_tree[i]
+                if pre_skill != skill[:skill.index(skill_tree[i])+1]:
+                    flag = 0
+                    break
+   
+        if flag == 1:
             answer += 1
-            continue
-
-        p = skill.index(tmp[0])
- 
-        if skill[p:len(tmp)] == tmp:
-            answer += 1
+                
 
     return answer
 
-print(solution("ABCDE", ["BA","AB", "CBADF", "AECB", "BDA"]))
+print(solution("CBD", [ "CBADF"]))
 print(solution("BDC", ["AAAABACA"]),0)
 print(solution("CBDK", ["CB", "CXYB", "BD", "AECD", "ABC", "AEX", "CDB", "CBKD", "IJCB", "LMDK"]), 4)
 print(solution("CBD", ["C", "D", "CB", "BDA"]), 2)
--- programmers/SummerWinterCoding2018/스티커모으기.py
+++ programmers/SummerWinterCoding2018/스티커모으기.py
@@ -1,33 +1,54 @@
 
 
+# def solution(sticker):
+#     answer = 0
+#     n = len(sticker)
+#     d1 = [0] * (n)
+#     d2 = [0] * (n)
+              
+
+#     if len(sticker) <= 3:
+#         return max(sticker)
+
+
+#     sticker1 = sticker[1:]
+#     sticker1.insert(0, 0 )
+#     sticker2 = sticker[:-1]
+#     sticker2.insert(0, 0 )
+#     d1[1] = sticker1[1]
+#     d1[2] = sticker1[2]
+#     d2[1] = sticker2[1]
+#     d2[2] = sticker2[2]
+#     for i in range(1, n):
+#         d1[i] = max(sticker1[i] + d1[i - 2], d1[i - 1])
+#         d2[i] = max(sticker2[i] + d2[i - 2], d2[i - 1])
+    
+
+
+#     answer = max(d1[-1], d2[-1])
+    
+
+#     return answer
+
 def solution(sticker):
     answer = 0
-    n = len(sticker)
-    d1 = [0] * (n)
-    d2 = [0] * (n)
-
 
     if len(sticker) <= 3:
         return max(sticker)
-
-
-    sticker1 = sticker[1:]
-    sticker1.insert(0, 0 )
-    sticker2 = sticker[:-1]
-    sticker2.insert(0, 0 )
-    d1[1] = sticker1[1]
-    d1[2] = sticker1[2]
-    d2[1] = sticker2[1]
-    d2[2] = sticker2[2]
-    for i in range(1, n):
-        d1[i] = max(sticker1[i] + d1[i - 2], d1[i - 1])
-        d2[i] = max(sticker2[i] + d2[i - 2], d2[i - 1])
     
-
-
-    answer = max(d1[-1], d2[-1])
+    dp1 = [0] * (len(sticker) -1)
+    dp1[0], dp1[1] = sticker[0], sticker[1]
     
-
+    dp2 = [0] * (len(sticker) -1)
+    dp2[0], dp2[1] = sticker[1], sticker[2]
+    
+    for i in range(2, len(sticker)-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i])
+        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i + 1])
+    
+    answer = max(dp1[-1], dp2[-1])
+    
     return answer
+# print(solution([1, 3, 2, 5, 4]))
 
-print(solution([1, 3, 2, 5, 4]))
+print(solution([5, 1, 16, 17, 16] ))
