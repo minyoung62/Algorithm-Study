bd4a430d6ffa93c4723f815f488b2edcfef709b4
minyoung <61530368+minyoung62@users.noreply.github.com>
2022-05-18 11:05:47 +0900

    [Bronze V] Title: 검증수, Time: 216 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        for(int i = 0; i < 5; i ++) {
+            sum += Math.pow(sc.nextDouble(), 2);
+        }
+        System.out.println((int)sum%10);
+
+    }
+
+
+
+
+}
