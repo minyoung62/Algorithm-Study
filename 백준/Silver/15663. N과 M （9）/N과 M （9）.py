9dee8e9a2d7518ee59c4fa22a5988356982103aa
minyoung <61530368+minyoung62@users.noreply.github.com>
2022-10-07 19:32:22 +0900

    [Silver II] Title: N과 M (9), Time: 160 ms, Memory: 117172 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -4,25 +4,26 @@ a = list(map(int, input().split()))
 
 a.sort()
 selected = list()
+
+
+a.sort()
+selected = list()
 isUsed = set()
 used = [0] * 10
-def choose(cnt, cntval):
+def choose(cnt):
   if cnt == m:
-
-    tmp = "".join(list(map(str, selected)))
-    if tmp not in isUsed:
-      print(*selected)
-      isUsed.add(tmp)
+    print(*selected)
     return
-
+  remember = -1
   for i in range(n):
-    if used[i]:
+    if used[i] or remember == a[i]:
       continue
     used[i] = 1
+    remember = a[i]
     selected.append(a[i])
-    choose(cnt+1, cntval+1)
+    choose(cnt+1)
     selected.pop()
     used[i] = 0
 
 
-choose(0, 0)
+choose(0)
