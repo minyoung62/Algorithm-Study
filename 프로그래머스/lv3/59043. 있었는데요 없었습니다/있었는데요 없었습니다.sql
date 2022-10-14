27055c3334b869eeb5ed356be1c8eeaec842c424
minyoung <61530368+minyoung62@users.noreply.github.com>
2022-10-15 00:58:59 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -4,3 +4,10 @@ join animal_outs as o
 on i.animal_id = o.animal_id
 where i.datetime > o.datetime
 order by i.datetime
+
+# SELECT 
+# ins.animal_id, ins.name -- , ins.datetime, outs.datetime, datediff(ins.datetime, outs.datetime)
+# from animal_ins as ins
+# inner join animal_outs as outs on ins.animal_id = outs.animal_id
+# where datediff(outs.datetime, ins.datetime) < 0
+# order by ins.datetime;
